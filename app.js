05702f1f018a9a6b8e31fff6e94ccbfde140c044
Sean Liu <siang720@gmail.com>
2020-05-07 16:58:31 +0800

    remove unused code

--- app.js
+++ app.js
@@ -4,18 +4,11 @@ const db = require('./models')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
-const passport = require('./config/passport')
 
 app.use(bodyParser.urlencoded({ extended: true }))
-
-// setup passport
-app.use(passport.initialize())
-app.use(passport.session())
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
 
-require("./routes")(app, passport);
-
-// module.exports = app;
+require("./routes")(app);
