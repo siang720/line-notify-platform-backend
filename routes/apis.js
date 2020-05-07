3a7d17f880f0a27884e6c5ae8a32cece71df038c
Sean Liu <siang720@gmail.com>
2020-05-07 16:18:44 +0800

    POST /api/signin (return jwt)

--- routes/apis.js
+++ routes/apis.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const userController = require('../controllers/userController')
 
 router.get('/init', (req, res) => {
   return res.json({ message: 'Project init' })
 })
+
+router.post('/signin', userController.signIn)
+router.post('/signup', userController.signUp)
 
 module.exports = router;
