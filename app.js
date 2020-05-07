8ce8014e6542aa5b7a4dd05c7d8002f45050c1a0
Sean Liu <siang720@gmail.com>
2020-05-07 14:16:48 +0800

    project init

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express")
+const db = require('./models')
 const app = express()
 const port = 3000
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
+
+require("./routes")(app);
+
+// module.exports = app;
