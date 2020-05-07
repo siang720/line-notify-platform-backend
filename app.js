a1df59395fdf405f1cdd30825d84f5cd9845471f
Sean Liu <siang720@gmail.com>
2020-05-07 16:18:44 +0800

    POST /api/signin (return jwt)

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express")
 const db = require('./models')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+require('dotenv').config()
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
