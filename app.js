181d3d5565b7d15a23e01616c13998c655cf8f97
Sean Liu <siang720@gmail.com>
2020-05-11 17:28:19 +0800

    let json accepted by body parser and add cors

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 require('dotenv').config()
 const express = require("express")
 const db = require('./models')
+const cors = require('cors')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
 
+// cors 的預設為全開放
+app.use(cors())
+
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
