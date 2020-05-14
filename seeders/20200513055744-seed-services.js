7a06e6624838efaee74468ade6e0dbdc999e43ef
Sean Liu <siang720@gmail.com>
2020-05-14 16:52:30 +0800

    renew seeders

--- seeders/20200513055744-seed-services.js
+++ seeders/20200513055744-seed-services.js
@@ -7,6 +7,8 @@ module.exports = {
       clientId: '2WTIpO7PB5jBVvOGN2Foli',
       clientSecret: 'F1E1P4jSjezlG54m9KM3PZqw3Kh3nutq53CDoCEDuSH',
       UserId: 1,
+      subscriptURL: "https://notify-bot.line.me/oauth/authorize?response_type=code&client_id=2WTIpO7PB5jBVvOGN2Foli&redirect_uri=http://localhost:3000/services/1/callback&scope=notify&state=state",
+      callbackURL: "http://localhost:3000/services/1/callback",
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
@@ -14,6 +16,8 @@ module.exports = {
       clientId: '6MZ8X4qKJZ6XvX0aztO3uy',
       clientSecret: 'WyX6R8GWQbXeEpTipEu7BJRnMMEVSkS22U2rdFZUjQi',
       UserId: 2,
+      subscriptURL: "https://notify-bot.line.me/oauth/authorize?response_type=code&client_id=6MZ8X4qKJZ6XvX0aztO3uy&redirect_uri=http://localhost:3000/services/2/callback&scope=notify&state=state",
+      callbackURL: "http://localhost:3000/services/2/callback",
       createdAt: new Date(),
       updatedAt: new Date()
     }
