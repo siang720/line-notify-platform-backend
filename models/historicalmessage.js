d048d68d7c6bf876fe0038986bfca762eca1436f
Sean Liu <siang720@gmail.com>
2020-05-14 14:19:23 +0800

    add column to historical message model

--- models/historicalmessage.js
+++ models/historicalmessage.js
@@ -3,7 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   const HistoricalMessage = sequelize.define('HistoricalMessage', {
     message: DataTypes.STRING,
     sendTime: DataTypes.DATE,
-    ServiceId: DataTypes.INTEGER
+    ServiceId: DataTypes.INTEGER,
+    sendNum: DataTypes.INTEGER
   }, {});
   HistoricalMessage.associate = function (models) {
     HistoricalMessage.belongsTo(models.Service)
