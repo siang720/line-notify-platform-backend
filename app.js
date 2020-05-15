2cc0d48e42b2145c4d4575227b9571d0de2a4cd6
Sean Liu <siang720@gmail.com>
2020-05-15 17:30:12 +0800

    add swagger-jsdoc and swagger-ui-express package

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ const cors = require('cors')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    swagger: '2.0', // Specification (optional, defaults to swagger: '2.0')
+    info: {
+      title: 'LINE-Notify-Platform', // Title (required)
+      version: '1.0.0', // Version (required)
+    },
+  },
+  // Path to the API docs
+  apis: ['./api-docs/swagger.yaml'],
+};
+
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerSpec = swaggerJSDoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // cors 的預設為全開放
 app.use(cors())
